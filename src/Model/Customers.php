45fc36a7082873455e7fc4df397c1f8c77269560
arkaadiana <220040193@stikom-bali.ac.id>
2024-04-30 20:01:04 +0800

    remove country_id in Customers.php

--- src/Model/Customers.php
+++ src/Model/Customers.php
@@ -23,9 +23,9 @@ class Customers {
     public function addCustomer($data) {
         $stmt = $this->pdo->prepare("
             INSERT INTO customers 
-            (first_name, last_name, gender, email, phone_number, address, education, occupation, date_of_birth, monthly_income, credit_score, marital_status, country_id) 
+            (first_name, last_name, gender, email, phone_number, address, education, occupation, date_of_birth, monthly_income, credit_score, marital_status) 
             VALUES 
-            (:first_name, :last_name, :gender, :email, :phone_number, :address, :education, :occupation, :date_of_birth, :monthly_income, :credit_score, :marital_status, :country_id)
+            (:first_name, :last_name, :gender, :email, :phone_number, :address, :education, :occupation, :date_of_birth, :monthly_income, :credit_score, :marital_status)
         ");
 
         $stmt->execute([
@@ -41,7 +41,6 @@ class Customers {
             'monthly_income' => $data['monthly_income'],
             'credit_score' => $data['credit_score'],
             'marital_status' => $data['marital_status'],
-            'country_id' => $data['country_id']
         ]);
     }
 
@@ -60,7 +59,6 @@ class Customers {
             monthly_income = :monthly_income, 
             credit_score = :credit_score, 
             marital_status = :marital_status, 
-            country_id = :country_id 
             WHERE customer_id = :id
         ");
 
@@ -78,7 +76,6 @@ class Customers {
             'monthly_income' => $data['monthly_income'],
             'credit_score' => $data['credit_score'],
             'marital_status' => $data['marital_status'],
-            'country_id' => $data['country_id']
         ]);
     }
 
